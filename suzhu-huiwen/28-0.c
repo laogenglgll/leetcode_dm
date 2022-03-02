1d0c45d9ab736faee8e87735127f0a8b855fd688
laogeng <laogenglg@outlook.com>
2022-03-02 09:48:56 +0800

    2

--- suzhu-huiwen/28-0.c
+++ suzhu-huiwen/28-0.c
@@ -1,5 +1,5 @@
 /*
-
+就加了一个判断语句，结构时间减少一半。。。。。。。。。。
 O()
 O()
 */
@@ -14,5 +14,38 @@ int main()
 }
 
 int strStr(char * haystack, char * needle){
-   12
+    int i = 0,j = 0;
+    int haystacklong = strlen(haystack);
+    int needlelong = strlen(needle);
+    if(needlelong == 0) return 0;
+    for(i = 0;i < haystacklong;i++)
+    {
+        for(j = 0;j < needlelong;j++)
+        {
+            if(haystack[i + j] != needle[j])break;
+        }
+        if(j == needlelong - 1) return i;
+    }
+    return -1;
 }
+
+
+/*
+int strStr(char * haystack, char * needle){
+    int i = 0,j = 0;
+    int haystacklong = strlen(haystack);
+    int needlelong = strlen(needle);
+    if(needlelong == 0) return 0;
+    for(i = 0;i < haystacklong;i++)
+    {
+        if(i > (haystacklong - needlelong)) return -1;
+        for(j = 0;j < needlelong;j++)
+        {
+            if(haystack[i + j] != needle[j])break;
+        }
+        //printf("%d",i);
+        if(j == needlelong) return i;
+    }
+    return -1;
+}
+*/
