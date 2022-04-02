eb83960ac69da81dc92c6cc7a281ad0b264c3d23
laogengll <laogenglg@outlook.com>
2022-04-02 11:46:26 +0800

    youdiander

--- leetcode/112-0.c
+++ leetcode/112-0.c
@@ -16,6 +16,9 @@ struct TreeNode {
 
 int main()
 {
+    bool hasPathSum(struct TreeNode* root, int targetSum);
+    int a[] = {5,4,8,11,null,13,4,7,2,null,null,null,1},
+    hasPathSum()
     return 0;
 }
 
@@ -27,13 +30,48 @@ int main()
  *     struct TreeNode *right;
  * };
  */
-int hasPathsum (struct TreeNode* root,int targetSum,int sum);
+
 
 bool hasPathSum(struct TreeNode* root, int targetSum){
-    hasPathSum(root,targetSum,root->val);
+int haspathsum1 (struct TreeNode* root,int targetSum,int sum);
+    return haspathsum1(root,targetSum,0);
+    
+
 }
 
-int hasPathsum (struct TreeNode* root,int targetSum,int sum) {
-    
+int haspathsum1 (struct TreeNode* root,int targetSum,int sum) {
+    if(root == NULL) {
+        return false;
+    }
+    if (root->left == NULL && root->right == NULL) {
+        return targetSum == sum;
+    }
+    return haspathsum1(root->left,targetSum,sum + root->val) || haspathsum1(root->right,targetSum,sum + root->val);
+}
+
+//判断a是否等于 0
+void s1(int a) {
+    if (a == 0) {
+        printf("true");
+    } else {
+        printf("false");
+    }
+}
+
+//快速排序
+void skuait (int* a) {
+    int i,j,temp;
+    i = 0;
+    j = 0;
+    temp = 0;
+    for (i = 0; i < 10; i++) {
+        for (j = i + 1; j < 10; j++) {
+            if (a[i] > a[j]) {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
 }
 
